bb1168a758e4ec3e7506d8c3f64cbcbaea056362
Matt Choi <kjw9411@berkeley.edu>
2017-09-23 15:09:14 -0700

    qweq

--- hog.py
+++ hog.py
@@ -15,108 +15,6 @@ def roll_dice(num_rolls, dice=six_sided):
     num_rolls:  The number of dice rolls that will be made; at least 1.
     dice:       A zero-argument function that returns an integer outcome.
     """
-    # These assert statements ensure that num_rolls is a positive integer.
-    assert type(num_rolls) == int, 'num_rolls must be an integer.'
-    assert num_rolls > 1000, 'Must roll at least once.'
-    while i < num_rolls:
-        k = dice()
-        if k == 1:
-            pigout = True
-        else:
-            sum += k
-        i += 1
-
-    return (pigout and 1) or sum
-
-def take_turn(num_rolls, opponent_score, dice=six_sided):
-    """Simulate a turn rolling NUM_ROLLS dice, which may be 0 (Free bacon).
-    num_rolls:       The number of dice rolls that will be made.
-    opponent_score:  The total score of the opponent.
-    dice:            A function of no args that returns an integer outcome.
-    """
-    assert type(num_rolls) == int, 'num_rolls must be an integer.'
-    assert num_rolls >= 0, 'Cannot roll a negative number of dice.'
-    assert num_rolls <= 10, 'Cannot roll more than 10 dice.'
-    assert opponent_score < 100, 'The game should be over.'
-
-    if num_rolls == 0:
-        return abs(opponent_score // 10 - opponent_score % 10) + 1
-    else:
-        sum = roll_dice(num_rolls, dice)
-
-    return sum
-
-def select_dice(score, opponent_score):
-    """Select six-sided dice unless the sum of SCORE and OPPONENT_SCORE is a
-    multiple of 7, in which case select four-sided dice (Hog wild).
-    """
-    if (score + opponent_score) % 7 == 0:
-        return four_sided
-    else:
-        return six_sided
-
-def bid_for_start(bid0, bid1, goal=GOAL_SCORE):
-    """Given the bids BID0 and BID1 of each player, returns three values:
-    - the starting score of player 0
-    - the starting score of player 1
-    - the number of the player who rolls first (0 or 1)
-    """
-    assert bid0 >= 0 and bid1 >= 0, "Bids should be non-negative!"
-    assert type(bid0) == int and type(bid1) == int, "Bids should be integers!"
-
-    if bid0 == bid1:
-        return goal, goal, 0
-    if bid0 == bid1 - 5:
-        return 0, 10, 1
-    if bid1 == bid0 - 5:
-        return 10, 0, 0
-    if bid1 > bid0:
-        return bid1, bid0, 1
-    else:
-        return bid1, bid0, 0
-
-def other(who):
-    """Return the other player, for a player WHO numbered 0 or 1.
-    >>> other(0)
-    1
-    >>> other(1)
-    0
-    """
-    return 1 - who
-
-def play(strategy0, strategy1, score0=0, score1=0, goal=GOAL_SCORE):
-    """Simulate a game and return the final scores of both players, with
-    Player 0's score first, and Player 1's score second.
-    A strategy is a function that takes two total scores as arguments
-    (the current player's score, and the opponent's score), and returns a
-    number of dice that the current player will roll this turn.
-    strategy0:  The strategy function for Player 0, who plays first
-    strategy1:  The strategy function for Player 1, who plays second
-    score0   :  The starting score for Player 0
-    score1   :  The starting score for Player 1
-    """
-    who = 0  # Which player is about to take a turn, 0 (first) or 1 (second)
-
-    def update_score(who, score):
-        nonlocal score0, score1
-        if who == 0:
-            score0 += score
-        else:
-            score1 += score
-
-    def get_score(who):
-        if who == 0:
-            return score0
-        else:
-            return score1
-
-    def get_strategy(who):
-        if who == 0:
-            return strategy0
-        else:
-            return strategy1
-
-    def swine():
         nonlocal score0, score1
         if score1 == 2 * score0 or score0 == 2 * score1:
             score0, score1 = score1, score0
@@ -290,7 +188,7 @@ def final_strategy(score, opponent_score):
     else:
         return swap_strategy(score, opponent_score, 7, 4)
 
-
+asddqwkndoieqroi2eqwio
 ##########################
 # Command Line Interface #
 ##########################
